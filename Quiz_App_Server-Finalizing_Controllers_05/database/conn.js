dbe7dbd4192a3a3d4a8187fa9150a87d2c471e0a
Ayush Kedia <83538020+ayushkedia05@users.noreply.github.com>
2022-12-29 00:37:08 +0530

    Update conn.js

--- Quiz_App_Server-Finalizing_Controllers_05/database/conn.js
+++ Quiz_App_Server-Finalizing_Controllers_05/database/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export default async function connect(){
-    await mongoose.connect('mongodb+srv://ayushkedia:kedi%40YUSH05@cluster0.3bemq.mongodb.net/natours')
+    await mongoose.connect('')
     console.log("Database Connected")
 }
